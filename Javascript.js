10718d6a9256f6322879709cea612aafa06d699b
Annes Muhamed <annes01@gmx.de>
2024-01-30 21:11:42 +0100

    JSON Array Strucktur

---
+++ Javascript.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Welches der folgenden fünf Elemente ist für die Webentwicklung relevant?",
+        "answer_1": "SQL-Datenbank",
+        "answer_2": "Bilderbearbeitungssoftware",
+        "answer_3": "3D-Drucker",
+        "answer_4": "CSS-Stylesheet",
+        "right_answer": 4
+    },
+
+    {
+        "question": "Wann wurde die erste Webseite online gestellt?",
+        "answer_1": "6. August 1991",
+        "answer_2": "1. Januar 1874",
+        "answer_3": "24 November 2001",
+        "answer_4": "11 April 1970",
+        "right_answer": 1
+    },
+
+    {
+        "question": "Welcher der folgenden fünf ist ein Webbrowser?",
+        "answer_1": "Microsoft Excel",
+        "answer_2": "Mozilla Firefox",
+        "answer_3": "Adobe Photoshop",
+        "answer_4": "Java Virtual Machine",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Welches der folgenden fünf Markup-Sprachen wird häufig für die Strukturierung von Inhalten in Webseiten verwendet?",
+        "answer_1": "JSON",
+        "answer_2": "XML",
+        "answer_3": "YAML",
+        "answer_4": "CSV",
+        "right_answer": 2
+    },
+];
